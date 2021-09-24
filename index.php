32dc533c9970d3b20c0a8bbba6ea9682835c038b
Noof <noof.mohammed998@gmail.com>
2021-09-24 21:45:59 +0300

    Create a class inherits

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php
 class Fruit{
-     private  $fruit1 = "orange";
+     public  $fruit1 = "orange";
      private  $fruit2 = "banana";
 
    public function getAllFruit(){
+     //    return " I like $this->fruit1 and $this->fruit2";
           echo  " I like $this->fruit1 and $this->fruit2";
+     }
+     public function getFruit1(){
+     //     return "give me $this->fruit1";
+         echo "give me $this->fruit1";
      }
 }
 $fruit = new Fruit();
-$fruit->getAllFruit();
+$fruit->getAllFruit(). '<br>';
+$fruit->getFruit1();
+
+class Apple extends Fruit{
+     public $benefits="Weight Loss";
+}
+$fruit3 = new Apple;
+echo $fruit3->fruit1;
+echo $fruit3->benefits;
 ?>
