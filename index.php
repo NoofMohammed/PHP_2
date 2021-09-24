66dff25cee87a968baff3653d8d46a3d03ec0169
Noof <noof.mohammed998@gmail.com>
2021-09-24 17:47:51 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+class Fruit{
+     $fruit1 = "orange";
+     $fruit2 = "banana"
+
+   public function getAllFruit(){
+         return " I like $this->fruit1 and $this->fruit2"
+     }
+}
+getAllFruit()
+?>
