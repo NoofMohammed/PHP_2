2399923a5ca9f6cea1907fb74e0cde35e3899aaf
Noof <noof.mohammed998@gmail.com>
2021-09-25 12:20:19 +0300

    Add a constructor

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 class Fruit{
      public  $fruit1 = "orange";
      private  $fruit2 = "banana";
-
+     public  $fruit3 = "watermelon";
+     // Constructor class(1)
+     // public function __construct($fruit1, $fruit3){
+     //      $this->fruit1 = $fruit1;
+     //      $this->fruit3= $fruit3;
+     // }
 
    public function getAllFruit(){
      //    return " I like $this->fruit1 and $this->fruit2";
@@ -17,16 +22,29 @@ class Fruit{
 $fruit = new Fruit();
 $fruit->getAllFruit(). '<br>';
 $fruit->getFruit1();
+// use Constructor class(1)
+// $anotherFruit = new Fruit("Kiwi", "apple");
+// echo $anotherFruit->fruit1;
+// echo $anotherFruit->fruit3;
 
 class Apple extends Fruit{
      public $benefits="Weight Loss";
+     // Constructor class (2)
+     public function __construct($benefits){
+          $this->benefits = $benefits;
+     }
 }
-$fruit3 = new Apple;
-echo $fruit3->fruit1;
-echo $fruit3->benefits;
+// $favoriteFruits = new Apple();
+// echo $favoriteFruits->fruit1;
+// echo $favoriteFruits->benefits;
+//  use Constructor class (2)
+$useful = new Apple("fastDigestion");
+echo $useful->benefits;
+
 
 class Food{
      private $vegetable = "potato";
+     public $favoriteVegetable = "zucchini";
      // getters
      public function getVegetable(){
           return $this->vegetable;
@@ -35,11 +53,20 @@ class Food{
      public function setVegetable($newVegetable){
           $this->vegetable = $newVegetable;
      }
+
+     // Constructor class(3)
+     public function __construct($favoriteVegetable){
+          $this->favoriteVegetable = $favoriteVegetable;
+     }
 }
-$vegetable1= new Food;
-// getters
-echo $vegetable1->getVegetable();
-// setters
-echo $vegetable1->setVegetable("tomato");
-echo $vegetable1->getVegetable();
+// $vegetable1= new Food();
+// // getters
+// echo $vegetable1->getVegetable();
+// // setters
+// echo $vegetable1->setVegetable("tomato");
+// echo $vegetable1->getVegetable();
+
+// use Constructor class(3)
+$vegetables = new Food("cucumber");
+echo $vegetables->favoriteVegetable;
 ?>
